d783b09d9b0902f5a7744439dcecf3a19e0aeb7e
vharvey <vharvey302@gmail.com>
2019-04-07 14:55:18 -0400

    initial

---
+++ script/process-review.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Chuck Norris Action Jeans | Century</title>
+        <link href="../styles/style.css" rel="stylesheet" media="screen" />
+    </head>
+    <body>
+        <header>
+            <a id="homeButton" href="../index.html" title="Go to Home Page"><h1>Chuck Norris <span>Action Jeans</span></h1></a>
+            <nav class="global">
+                <ul>
+                    <li><a href="../info.html" title="Learn more about this product">More Info</a></li>
+                    <li><a href="../reviews.html" title="View Customer Reviews">Reviews</a></li>
+                    <li><a href="../order.html" title="Click Here to Order now">Order Now</a></li>
+                    <li><a class="../current" href="../contact.html" title="Get Customer Support">Contact</a></li>
+                    <li><a href="../century.html" title="Learn more about Century LLC">Century LLC</a></li>
+                </ul>
+            </nav>
+		</header>
+		<main>
+        <img id="stand" src="../images/chuck-norris-stand-1.png" alt="Chuck Norris" />
+        <div class="content">
+            <img class="img-process" src="../images/century.png" alt="Century Logo" width=196 height=123 />
+            <img class="img-process" src="../images/cn-logo.png" alt="Chuck Norris Logo" width=134 height=122 />
+            <h2>Thanks for your product review!</h2>
+            <div class="copy">
+                <p>Your review will appear on our site shortly! Thanks for choosing Chuck Norris Action Jeans!</p>
+            </div>
+        </div>
+	</main>
+	<footer>
+        <p><img src="../images/century-sm.png" alt="Century Athletic Logo" height=63 width=100 /><small><a href="https://www.centurymartialarts.com/" title="Go to the Century Martial Arts Official Site">Century Martial Arts Supply, LLC.</a> All rights reserved. All audio, visual and textual content on this site are protected by trademark rights, copyrights and other rights owned by Century LLC’s licensors, licensees, suppliers and accounts. Use of this site signifies your agreement to the Terms and Conditions of Use.<br>Site created by Vivian Harvey for COMP 1002, Fall 2017.</small></p>
+</footer>
+</body>
+</html>
